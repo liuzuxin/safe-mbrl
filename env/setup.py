d9daf9bab5c0b6afc5dcc2bf523211281e597d3b
liuzuxin <zuxin1997@gmail.com>
2022-04-21 15:33:00 -0400

    add safety gym env installation script

--- env/setup.py
+++ env/setup.py
@@ -11,10 +11,9 @@ setup(
     name='safety_gym',
     packages=['safety_gym'],
     install_requires=[
-        'gym~=0.15.3',
-        'joblib~=0.14.0',
+        'gym',
+        'numpy',
         'mujoco_py==2.0.2.7',
-        'numpy~=1.17.4',
-        'xmltodict~=0.12.0',
+        'xmltodict',
     ],
 )
